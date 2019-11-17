e035f779654725856a41813cf8753be2c6077f6d
Ahmed Yehia GPEL <ahmed.yehia.1177@gmail.com>
2019-11-17 21:27:13 +0100

    Move SGEngine in premake to a higher order

--- premake5.lua
+++ premake5.lua
@@ -8,45 +8,6 @@ IncludeDir["GLFW"] = "SG-Engine/thirdparty/GLFW/include"
 
 include "SG-Engine/thirdparty/GLFW"
 
-project "Playground"
-	location "Playground"
-	kind "ConsoleApp"
-	language "C++"
-	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
-
-	files	{
-		"%{prj.name}/src/**.h",
-		"%{prj.name}/src/**.cpp"
-	}
-	includedirs{
-		"SG-Engine/thirdparty/spdlog/spdlog-include",
-		"SG-Engine/src",
-		"SG-Engine/src/SGEngine/"
-
-	}
-	links "SG-Engine"
-
-	filter "system:windows"
-		cppdialect "C++17"
-		staticruntime "On"
-		systemversion "latest"
-		defines "SGE_PLATFORM_WINDOWS"
-
-	filter "configurations:Debug"
-		defines "SGE_DEBUG"
-		symbols "On"
-
-	filter "configurations:Release"
-		defines "SGE_RELEASE"
-		optimize "On"
-
-	filter "configurations:Distribute"
-		defines "SGE_DISTRIBUTE"
-		optimize "On"
-
-	dependson "SG-Engine"
-
 project "SG-Engine"
 	location "SG-Engine"
 	kind "SharedLib"
@@ -92,3 +53,41 @@ project "SG-Engine"
 		defines "SGE_DISTRIBUTE"
 		optimize "On"
 
+project "Playground"
+	location "Playground"
+	kind "ConsoleApp"
+	language "C++"
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+	includedirs{
+		"SG-Engine/thirdparty/spdlog/spdlog-include",
+		"SG-Engine/src",
+		"SG-Engine/src/SGEngine/"
+
+	}
+	links "SG-Engine"
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+		defines "SGE_PLATFORM_WINDOWS"
+
+	filter "configurations:Debug"
+		defines "SGE_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "SGE_RELEASE"
+		optimize "On"
+
+	filter "configurations:Distribute"
+		defines "SGE_DISTRIBUTE"
+		optimize "On"
+
+	dependson "SG-Engine"
